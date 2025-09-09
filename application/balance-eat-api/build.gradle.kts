2970dc068184cc679b4bafe4e5485459b46d2c5e
Moon-Jang <roby8502@naver.com>
2025-09-10 07:47:02 +0900

    [chore] 스웨거 설정 변경

--- application/balance-eat-api/build.gradle.kts
+++ application/balance-eat-api/build.gradle.kts
@@ -1,3 +1,7 @@
+import groovy.lang.Closure
+import io.swagger.v3.oas.models.servers.Server
+import org.gradle.internal.declarativedsl.intrinsics.listOf
+
 plugins {
     kotlin("plugin.spring")
     kotlin("plugin.jpa")
@@ -38,4 +42,28 @@ tasks.getByName("jar") {
     enabled = false
 }
 
-openapi3
+val testForApiDocs by tasks.registering(Test::class) {
+    useJUnitPlatform()
+
+    // 이 태스크는 ControllerTest만 실행하도록 필터링
+    filter {
+        includePatterns.add("**/*ControllerTest.kt")
+    }
+}
+
+//tasks.named("openapi3") {
+//
+//}
+
+
+openapi3 {
+    mkdir("src/main/resources/static/api-docs/balanceeat")
+    val outputDir = "src/main/resources/static/api-docs/balanceeat"
+
+    title = "BalanceEat API 명세서"  // 문서 제목
+    description = "BalanceEat API 명세서입니다."  // 문서 설명
+    version = "1.0.0"                  // API 버전
+    format = "yaml"                    // 출력 포맷 (json 또는 yaml)
+    outputDirectory = outputDir // 명세 파일이 생성될 위치
+    outputFileNamePrefix = "main"     // 파일명 접두사
+}
--- application/balance-eat-api/src/main/kotlin/org/balanceeat/api/config/SwaggerConfig.kt
+++ application/balance-eat-api/src/main/kotlin/org/balanceeat/api/config/SwaggerConfig.kt
@@ -1,80 +1,27 @@
 package org.balanceeat.api.config
 
-import io.swagger.v3.oas.models.Components
-import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.info.License
-import io.swagger.v3.oas.models.media.Content
-import io.swagger.v3.oas.models.media.MediaType
-import io.swagger.v3.oas.models.media.Schema
-import io.swagger.v3.oas.models.parameters.RequestBody
-import io.swagger.v3.oas.models.responses.ApiResponse
-import io.swagger.v3.oas.models.security.SecurityRequirement
-import io.swagger.v3.oas.models.security.SecurityScheme
-import io.swagger.v3.oas.models.servers.Server
+import org.springdoc.core.configuration.SpringDocConfiguration
+import org.springdoc.core.properties.SpringDocConfigProperties
+import org.springdoc.core.providers.ObjectMapperProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 class SwaggerConfig {
+    @Primary
+    @Bean
+    fun springDocConfiguration(): SpringDocConfiguration {
+        return SpringDocConfiguration()
+    }
 
     @Bean
-    fun openAPI(): OpenAPI {
-        return OpenAPI()
-            .info(
-                Info()
-                    .title("Balance Eat API")
-                    .version("1.0.0")
-            )
-            .servers(
-                listOf(
-                    Server()
-                        .url("http://localhost:8080")
-                        .description("Local Development Server"),
-                    Server()
-                        .url("https://api.balanceeat.com")
-                        .description("Production Server")
-                )
-            )
-            .addSecurityItem(
-                SecurityRequirement().addList("bearerAuth")
-            )
-            .components(
-                Components()
-                    .addSecuritySchemes(
-                        "bearerAuth",
-                        SecurityScheme()
-                            .type(SecurityScheme.Type.HTTP)
-                            .scheme("bearer")
-                            .bearerFormat("JWT")
-                            .description("JWT 토큰을 입력하세요")
-                    )
-                    .addRequestBodies(
-                        "defaultJsonRequestBody",
-                        RequestBody()
-                            .content(
-                                Content()
-                                    .addMediaType(
-                                        "application/json",
-                                        MediaType()
-                                            .schema(Schema<Any>().type("object"))
-                                    )
-                            )
-                            .required(true)
-                    )
-                    .addResponses(
-                        "defaultJsonResponse",
-                        ApiResponse()
-                            .description("Default JSON Response")
-                            .content(
-                                Content()
-                                    .addMediaType(
-                                        "application/json",
-                                        MediaType()
-                                            .schema(Schema<Any>().type("object"))
-                                    )
-                            )
-                    )
-            )
+    fun springDocConfigProperties(): SpringDocConfigProperties {
+        return SpringDocConfigProperties()
+    }
+
+    @Bean
+    fun objectMapperProvider(springDocConfigProperties: SpringDocConfigProperties): ObjectMapperProvider {
+        return ObjectMapperProvider(springDocConfigProperties)
     }
 }
